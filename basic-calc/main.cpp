1fb404b4d457cb21c03c033de9226a72b06af901
omynayak <manelomnayak099@gmail.com>
2025-05-29 16:00:22 +0530

    Added comments to the code

--- basic-calc/main.cpp
+++ basic-calc/main.cpp
@@ -1,19 +1,27 @@
+/*
+author: omynayak
+function and description: serves as a calculator that helps exercise essential programming practices like flow control,
+                            conditionals, unions and basic input/output
+*/
+
 #include <iostream>
 #include <stdbool.h>
 using namespace std;
 
+//Define a union that holds all the variables I will be using
+//Using a union since no two of these variables will need to exist simultaneously
 union choicesAndValues
 {
     char choice;
-    float tempVal;
     char operation;
     float secondVal;
 } cAndV;
 
 
-
+//Where all the magic happens
 int main()
 {
+    // Read and validate the input choice to begin or not
     cout << "Would you like to begin?(y/n): ";
     cin >> cAndV.choice;
     while (cAndV.choice != 'y' && cAndV.choice != 'n')
@@ -21,21 +29,26 @@ int main()
         cout << "Invalid input. Please try again: ";
         cin >> cAndV.choice;
     }
-
+    
+    // Start a while loop that contains the main part of the program with a bool flag to check if it is the first value being entered
     float values;
     bool flag = true;
-    while (cAndV.choice == 'y')
+    while (cAndV.choice == 'y') 
     {
         if (flag)
         {
             cout << "Enter the first value: ";
-            cin >> cAndV.tempVal;
-            values = cAndV.tempVal;
+            cin >> cAndV.secondVal;
+            values = cAndV.secondVal;
             flag = false;
         }
 
+        // The program continuously overwrites the "values" variable from here on, discarding any previous value stored by replacing it
+        // with an updated version complete with the computation performed on it with the other number entered
         cout << "Please choose the operation:(+ - / * % /c)";
         cin >> cAndV.operation;
+
+        // Apply operations based on the input character
         switch (cAndV.operation)
         {
         case '+':
@@ -84,16 +97,19 @@ int main()
             flag = true;
             break;
 
+        // The default condition in case the input was not in the list
         default:
             cout << "Invalid operation. Try again.";
             continue;
         }
 
+        // If the calculator value was NOT cleared and reset, display the current output
         if (!(flag))
         {
             cout << "The answer is: " << values << endl;
         }
 
+        // Take input for the choice variable that controls the while loop
         cout << "Would you like to continue?(y/n) ";
         cin >> cAndV.choice;
         if (cAndV.choice != 'y')
